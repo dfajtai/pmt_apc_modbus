3539f022b03d64f6644cd53806a253d5011f3de2
Daniel Fajtai <33656120+dfajtai@users.noreply.github.com>
2025-12-02 16:40:18 +0100

    Update apc_instrument.py
    simplification, removing unused, legacy sync methods

--- src/logic/apc_instrument.py
+++ src/logic/apc_instrument.py
@@ -10,9 +10,6 @@ from model.config_model import AppConfig
 from services.modbus_query import ModbusQuery
 
 
-from services.modbus_connection import ModbusConnection
-from services.modbus_handler import ModbusHandler
-
 from services.async_modbus_connection import AsyncModbusConnection
 from services.async_modbus_handler import AsyncModbusHandler
 
@@ -36,54 +33,17 @@ class PmtApcInstrument:
         NORMAL = 0
         ABNORMAL = 1
         
-    def __init__(self, relay: ModbusHandler | AsyncModbusHandler, logger: Optional[logging.Logger] = None):
-        if not (isinstance(relay,ModbusHandler) or isinstance(relay,AsyncModbusHandler)):
+    def __init__(self, relay: AsyncModbusHandler, logger: Optional[logging.Logger] = None):
+        if not isinstance(relay,AsyncModbusHandler):
             raise ValueError
         self.relay = relay
     
-        self.sampling_status = None
-        self.device_status = None
+        self.sampling_status = self.SamplingStatus.NOT_SAMPLING
+        self.device_status = self.DeviceStatus.NORMAL
+
         self.flow_rate = None
         self.logger: Optional[logging.Logger] = logger
-    
 
-    # ****************** SYNC ****************** 
-    # read status
-
-    def read_sampling_status(self) -> "PmtApcInstrument.SamplingStatus":
-        status_query = ModbusQuery("read_sampling_status",30164,1)
-        response = self.relay.read_input(status_query)
-        return PmtApcInstrument.SamplingStatus(response)
-    
-    def read_device_status(self):
-        status_query = ModbusQuery("read_sampling_status",30214,1)
-        response = self.relay.read_coil(status_query)
-        return PmtApcInstrument.DeviceStatus(response)
-
-    # control
-
-    def read_flow(self):
-        # SET flow not worked in modbus applications.
-        flow_query =  ModbusQuery("read_flow",30022,2,dtype="uint32")
-        response = self.relay.read_input(flow_query)
-        return response
-
-    def start_sampling(self):
-        control_query = ModbusQuery("control_sampling",2,1,writeable=True)
-        self.relay.write_coil(control_query,True)
-
-    def stop_sampling(self):
-        control_query = ModbusQuery("control_sampling",2,1,writeable=True)
-        self.relay.write_coil(control_query,False)
-
-    # data read
-
-    def read_channels(self, name_list = None):
-        pass
-        # TODO implement...
-
-
-    # ****************** ASYNC ****************** 
 
     async def async_read_sampling_status(self)-> "PmtApcInstrument.SamplingStatus":
         status_query = ModbusQuery("read_sampling_status",30164,1)
@@ -107,20 +67,29 @@ class PmtApcInstrument:
 
     # control
 
-    async def async_start_sampling(self):
+    async def async_start_sampling(self)-> bool:
         if self.logger:
             self.logger.debug("Starting sampling over MODBUS ....")
 
         control_query = ModbusQuery("control_sampling",2,1,writeable=True)
         
-        await self.relay.write_coil(control_query,True)
+        res = await self.relay.write_coil(control_query,True)
 
         if self.logger:
             self.logger.debug("Sampling status over MODBUS modified")
 
-    async def async_stop_sampling(self):
+        if res:
+            return True
+        
+        return False
+    
+
+    async def async_stop_sampling(self) -> bool:
         control_query = ModbusQuery("control_sampling",2,1,writeable=True)
-        await self.relay.write_coil(control_query,False)
+        res = await self.relay.write_coil(control_query,False)
+        if res:
+            return True
+        return False
 
     # read data
 
